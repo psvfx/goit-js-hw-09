ce465c875cf0f4d89d83e4a7bbe5dd0a2018352f
psvfx <psvfxinvest@gmail.com>
2023-06-24 20:13:54 +0300

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,26 @@
-console.lofg('Hello world');
-``;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startButtonEl.addEventListener('click1', () => {
+  startButtonEl.disabled = true;
+  stopButtonEl.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButtonEl.addEventListener('click', () => {
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+
+  clearInterval(intervalId);
+});
