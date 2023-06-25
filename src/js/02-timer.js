d92c22cac42e8f4de567f86ec3fedc39603c7484
psvfx <psvfxinvest@gmail.com>
2023-06-25 19:13:15 +0300

    adding completed task 2 and 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// Імпорт бібліотеки, що дозволяє користувачеві кросбраузерно вибрати кінцеву дату і час в одному елементі інтерфейсу:
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів:
+import 'flatpickr/dist/flatpickr.min.css';
+// Імпорт бібліотеки для неблокуючих сповіщень на стороні клієнта:
+import Notiflix from 'notiflix';
+
+// Налаштування для бібліотеки flatpickr, що дозволяє користувачеві вибрати дату та час:
+const options = {
+  enableTime: true, //дозволяє користувачеві вибирати час у вибірнику
+  time_24hr: true, //встановлює 24-годинний формат часу
+  defaultDate: new Date(), //встановлює поточну дату та час як значення за замовчуванням у вибірник
+  minuteIncrement: 1, // встановлює крок зміни хвилин у вибірнику на 1
+  // Логіка виконання при закритті вибору дати:
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    // Вивід повідомлення при невідповідності умови (дата не змінена):
+    if (selectedDate < currentDate) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+      return;
+    }
+
+    const startButtonEl = document.querySelector('[data-start]');
+    startButtonEl.disabled = false;
+  },
+};
+
+// Виклик функції бібліотеки з ідентифікацією з DOM елементом (поява календарного вибірника на інпуті):
+flatpickr('#datetime-picker', options);
+
+// Отримання посилань на елементи DOM:
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+let countdownInterval = null;
+// Додавання обробника події для кнопки "Start", який запускає таймер зворотного відліку:
+startButton.addEventListener('click', () => {
+  const selectedDate = flatpickr.parseDate(
+    document.querySelector('#datetime-picker').value
+  );
+  const currentDate = new Date();
+
+  if (selectedDate <= currentDate) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  }
+
+  startButton.disabled = true;
+
+  // Таймер зворотного відліку, який оновлює значення днів, годин, хвилин і секунд на елементах DOM кожну секунду:
+  countdownInterval = setInterval(() => {
+    const remainingTime = selectedDate - new Date();
+    const { days, hours, minutes, seconds } = convertMs(remainingTime);
+
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
+
+    if (remainingTime <= 0) {
+      clearInterval(countdownInterval);
+      startButton.disabled = false;
+    }
+  }, 1000);
+});
+
+// Функція, яка приймає кількість мілісекунд і повертає об'єкт з розрахунками днів, годин, хвилин і секунд:
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// Функція, яка приймає число і додає ведучий нуль, якщо число менше 10:
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,67 @@
+'use strict';
+// Підключення бібліотеки для неблокуючих сповіщень на стороні клієнта:
+import Notiflix from 'notiflix';
+
+//Функція повернення нового промісу випадковим чином із затримкою:
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    } else {
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    }
+  });
 }
+
+// Отримання посилання до форми:
+const formEl = document.querySelector('.form');
+
+// Прослуховування події відправки форми та виклику функції-обробника події, яка перевіряє введені дані форми:
+formEl.addEventListener('submit', function (event) {
+  // Скидання дефолтної поведінки
+  event.preventDefault();
+
+  // Зчитування значень полів форми:
+  const delayInput = formEl.elements.delay;
+  const stepInput = formEl.elements.step;
+  const amountInput = formEl.elements.amount;
+
+  // Перетворення на числа значень полів форми:
+  const firstDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  // // Цикл створення зазначеної кількості промісів з відповідними затримками та виводами повідомлень у консоль виконання методів then або catch:
+  // for (let i = 0; i < amount; i += 1) {
+  //   createPromise(i, firstDelay + step * i)
+  //     .then(({ position, delay }) => {
+  //       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  //     })
+  //     .catch(({ position, delay }) => {
+  //       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  //     });
+  // }
+
+  // Цикл створення зазначеної кількості промісів з відповідними затримками та виводами повідомлень користувачеві, використовуючи бібліотеку notiflix:
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i, firstDelay + step * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+
+  // Скидання в формі введених даних:
+  formEl.reset();
+});
